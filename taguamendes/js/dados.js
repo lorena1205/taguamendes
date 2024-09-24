baca3ef48317104880c79bfda17f6b531f5bdda1
Lorena Muniz Ferreira <37228727+lorena1205@users.noreply.github.com>
2024-09-24 11:28:41 -0300

    Add files via upload

---
+++ taguamendes/js/dados.js
@@ -0,0 +1,352 @@
+var MENU = {
+    "Destilados": [
+        {
+            "id": "beefeater",
+            "img": "./img/cardapio/destilados/beefeater.jpeg",
+            "name": "Beefeater",
+            "dsc": "Gin Beefeater London Dry 750ml",
+            "price": 130.00
+        },
+        {
+            "id": "kriskoff",
+            "img": "./img/cardapio/destilados/kriskoff.jpeg",
+            "name": "kriskoff",
+            "dsc": "Vodka Kriskof Tradicional 900Ml",
+            "price": 15.00
+        },
+        {
+            "id": "orloff",
+            "img": "./img/cardapio/destilados/orloff.jpg",
+            "name": "Orloff",
+            "dsc": "Texas Monthly's #1 BBQ Brisket",
+            "price": 199
+        },
+        {
+            "id": "smirnoff",
+            "img": "./img/cardapio/destilados/smirnoff.jpeg",
+            "name": "Smirnoff",
+            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+            "price": 40
+        },
+        {
+            "id": "Buchanans",
+            "img": "./img/cardapio/destilados/buchannans.JPG", 
+            "name": "Buchanans",
+            "dsc": "Whisky Buchanans 12 Anos 1000ml",
+            "price": 160
+        },
+        {
+            "id": "Buchanans",
+            "img": "./img/cardapio/destilados/buchannans.JPG", 
+            "name": "Buchanans",
+            "dsc": "Chivas Regal Whisky 12 anos Escocês 1L",
+            "price": 160
+        },
+
+    ],
+    "Cervejas": [
+        {
+            "id": "amstel",
+            "img": "./img/cardapio/Cervejas/amstel.jpg",
+            "name": "amstel",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 99
+        },
+        {
+            "id": "bhama-chopp",
+            "img": "./img/cardapio/Cervejas/bhama-chopp.JPG",
+            "name": "Bhama-chopp",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 49
+        },
+        {
+            "id": "bhama-duplo",
+            "img": "./img/cardapio/Cervejas/bhama-duplo.JPG",
+            "name": "Bhama Duplo",
+            "dsc": "Gott's Complete Cheeseburger Kit for 4",
+            "price": 99
+        },
+        {
+            "id": "Budweiser",
+            "img": "./img/cardapio/Cervejas/Budweiser.png",
+            "name": "Budweiser",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 99
+        },
+        {
+            "id": "heiniken-lata",
+            "img": "./img/cardapio/Cervejas/heiniken-lata.JPG",
+            "name": "Heiniken lata",
+            "dsc": "Shake Shack Shackburger® - 16 Pack",
+            "price": 89
+        },
+        {
+            "id": "heiniken-long",
+            "img": "./img/cardapio/Cervejas/heiniken-long.webp",
+            "name": "Heiniken long",
+            "dsc": "USDA Prime cervejas - Pack of 18 (8oz each)",
+            "price": 175.95
+        },
+        {
+            "id": "itaipava",
+            "img": "./img/cardapio/Cervejas/itaipava.jpg",
+            "name": "itaipava",
+            "dsc": "Double Stack Burger Kit for 4",
+            "price": 79
+        },
+        {
+            "id": "kaiser",
+            "img": "./img/cardapio/Cervejas/kaiser.JPG",
+            "name": "kaiser",
+            "dsc": "KAISER",
+            "price": 109
+        },
+        {
+            "id": "spaten",
+            "img": "./img/cardapio/Cervejas/spaten.jpg",
+            "name": "spaten",
+            "dsc": "Burger Au Poivre Kit - 4 Pack",
+            "price": 99
+        },
+    ],
+    "Diversos": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/cardapio/diversos/gelo-coco.jpg",
+            "name": "Gelo de coco",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 39.99
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "./img/cardapio/diversos/GELO.JPEG",
+            "name": "Gelo 5kg",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
+            "price": 39.95
+        },
+
+    ],
+    "Narguile": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img/cardapio/narguile/hong-kong-boba-tea-kit-for-6.63841de36d8e5edfafa13023fc303285.jpg",
+            "name": "New Territories",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+            "img": "./img/cardapio/narguile/guys-caliente-margaritas-for-12.ca8c6bc06b8f1039549385ffcebc749d.jpg",
+            "name": "Guy Fieri",
+            "dsc": "Guy's Caliente Margaritas for 12",
+            "price": 69
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup",
+            "img": "./img/cardapio/narguile/woodford-reserve-mint-julep-syrup.ef523ac7cbae5f4aba6b058207f490d2.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Woodford Reserve Mint Julep Syrup",
+            "price": 39
+        },
+        {
+            "id": "new-orleans-hurricane-mix",
+            "img": "./img/cardapio/narguile/new-orleans-hurricane-mix.4613584fc65cb0787024dd24d2a8f4b3.jpg",
+            "name": "Franco's Hurricane Mix",
+            "dsc": "New Orleans Hurricane Mix",
+            "price": 39
+        },
+        {
+            "id": "margarita-mix",
+            "img": "./img/cardapio/narguile/margarita-mix.bd48a000d589d3147b14790af3c33fcd.jpg",
+            "name": "Johnny Sanchez",
+            "dsc": "Margarita Mix",
+            "price": 59
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup-2-pack",
+            "img": "./img/cardapio/narguile/woodford-reserve-mint-julep-syrup-2-pack.0ac76063f151988113cbaabd0eaa829f.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
+            "price": 59
+        },
+        {
+            "id": "unicorn-parade-milkshake-kit-for-8",
+            "img": "./img/cardapio/narguile/unicorn-parade-milkshake-kit-for-2.9052d04c1cf25b29442048bd3e535f21.jpg",
+            "name": "New Territories",
+            "dsc": "Unicorn Parade Milkshake Kit for 8",
+            "price": 109
+        },
+        {
+            "id": "chickpea-chiller-kit-for-6",
+            "img": "./img/cardapio/narguile/chickpea-chiller-kit-for-6.4310765c71ba524b5462ea9330d32446.jpg",
+            "name": "The Hummus & Pita Co.",
+            "dsc": "Chickpea Chiller Kit for 6",
+            "price": 89
+        },
+        {
+            "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
+            "img": "./img/cardapio/narguile/old-honey-barn-mint-julep-mixer-200ml.e0b131d6d9b69963706b43fd4334ab74.jpg",
+            "name": "Old Honey Barn Mint Julep",
+            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
+            "price": 25
+        },
+        {
+            "id": "kentucky-derby-mint-julep-gift-set",
+            "img": "./img/cardapio/narguile/kentucky-derby-mint-julep-gift-set.79720eda4e9c8e3fcf9ecb5c79827f2c.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Kentucky Derby Mint Julep Gift Set",
+            "price": 59.95
+        },
+        {
+            "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
+            "img": "./img/cardapio/narguile/weekender-charleston-bloody-mary-mix-bold-and-spicy.c372868c9937e407a299a22001e210e2.jpg",
+            "name": "Charleston Beverage Company",
+            "dsc": "Weekender | Charleston Bloody Mary Mix Bold & Spicy",
+            "price": 39.95
+        },
+        {
+            "id": "nola-cold-brew-concentrate-bag-in-box",
+            "img": "./img/cardapio/narguile/nola-cold-brew-concentrate-bag-in-box.3df6fde8bd83f29235565984ae8ed22b.jpg",
+            "name": "Grady's Cold Brew",
+            "dsc": "NOLA Cold Brew Concentrate Bag-in-Box",
+            "price": 40
+        }
+    ],
+    "Refrigerantes": [
+        {
+            "id": "coca1",
+            "img": ".\img\cardapio\refrigerantes\coca-retornavel.jpg",
+            "name": "coca cola",
+            "dsc": "2 Lou Malnati's Deep Dish refrigerantes",
+            "price": 67.99
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": ".\img\cardapio\refrigerantes\coca-retornavel.jpg",
+            "name": "Bartolini's",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 139
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": ".\img\cardapio\refrigerantes\coca-retornavel.jpg",
+            "name": "Zuppardi's Apizza",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 79
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-refrigerantes",
+            "img": ".\img\cardapio\refrigerantes\coca-retornavel.jpg",
+            "name": "Lou Malnati's Pizza",
+            "dsc": "6 Lou Malnati's Deep Dish refrigerantes",
+            "price": 116.99
+        },
+        {
+            "id": "wood-fired-refrigerantes-best-seller-4-pack",
+            "img": ".\img\cardapio\refrigerantes\coca-retornavel.jpg",
+            "name": "Pizzeria Bianco",
+            "dsc": "Wood Fired refrigerantes Best Seller - 4 Pack",
+            "price": 129
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+            "img": ".\img\cardapio\refrigerantes\coca-retornavel.jpg",
+            "name": "Bartolini's",
+            "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
+            "price": 139
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+            "img": ".\img\cardapio\refrigerantes\coca-retornavel.jpg",
+            "name": "Emmy Squared",
+            "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
+            "price": 89
+        },
+    ],
+    "sucos": [
+        {
+            "id": "california-reserve-filet-mignon-sucos-gift-box",
+            "img": "./img/cardapio/sucos/california-reserve-filet-mignon-sucos-gift-box.bf226e317aad85f47897ae7e325f790d.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon sucos Gift Box",
+            "price": 129
+        },
+        {
+            "id": "sucos-and-cakes-date-night-dinner-for-2",
+            "img": "./img/cardapio/sucos/valentines-sucos-and-cakes-dinner-for-2.0c63dab635eed46209455dc33cd25ea8.jpg",
+            "name": "Chesapeake Bay Gourmet",
+            "dsc": "sucos and Cakes Date Night Dinner for 2",
+            "price": 129
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "./img/cardapio/sucos/holiday-sampler-9-pack.2de75ca80282ffe9d064eb757ff7a5a1.jpg",
+            "name": "Saltbrick Prime",
+            "dsc": "Chef Matt's Steak Sampler for 10-12",
+            "price": 179
+        },
+        {
+            "id": "bone-in-rib-steak",
+            "img": "./img/cardapio/sucos/bone-in-rib-steak.b13d9d4233035767605f0de9acdce1ab.jpg",
+            "name": "Old Homestead Steakhouse",
+            "dsc": "Bone-in Rib Steak",
+            "price": 159
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+            "img": "./img/cardapio/sucos/american-wagyu-gold-grade-sirloins.040dbeb8f8e615b91fa7e513e3dc089c.jpg",
+            "name": "Snake River Farms",
+            "dsc": "American Wagyu Gold Grade Top Sirloins",
+            "price": 119
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+            "img": "./img/cardapio/sucos/peter-luger-steak-pack-b.9feb0300e6be2dfecfa314f2006a2183.jpg",
+            "name": "Peter Luger Steak House",
+            "dsc": "Peter Luger Porterhouse sucos",
+            "price": 215.95
+        },
+        {
+            "id": "ribeye-prime-steak-gift-box",
+            "img": "./img/cardapio/sucos/ribeye-prime-steak-gift-box.e74cb016baabbb2df73861de8150f29c.jpg",
+            "name": "Churchill's Steakhouse",
+            "dsc": "Ribeye Prime Steak Gift Box",
+            "price": 229
+        },
+        {
+            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
+            "img": "./img/cardapio/sucos/usda-prime-black-angus-filet-mignon-barrel-cut.6ee213799e7d1848763d12edca18e3b1.jpg",
+            "name": "Pat LaFrieda Meats",
+            "dsc": "Dry-Aged USDA Prime Black Angus Porterhouse Steak - 2 Pack",
+            "price": 96.7
+        },
+        {
+            "id": "california-reserve-ribeye-steak",
+            "img": "./img/cardapio/sucos/california-reserve-ribeye-steak-12-oz.d367c5ae72dd9f89e170662104bef4fc.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Ribeye Steak - 12 oz",
+            "price": 32
+        },
+        {
+            "id": "dry-aged-boneless-ribeye-steak-dinner-kit-for-4",
+            "img": "./img/cardapio/sucos/dry-aged-boneless-ribeye-steak-dinner-for-4.81c3bdc05fe6bdb2c2214709863120e0.jpg",
+            "name": "Chef Francis Mallmann",
+            "dsc": "Dry-Aged Boneless Ribeye Steak Dinner Kit for 4",
+            "price": 225
+        },
+        {
+            "id": "california-reserve-filet-mignon-steak",
+            "img": "./img/cardapio/sucos/california-reserve-filet-mignon-steak.ff15071964ec8141d30c2ba05fb117e0.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steak",
+            "price": 22
+        },
+        {
+            "id": "mesquite-smoked-peppered-beef-tenderloin",
+            "img": "./img/cardapio/sucos/mesquite-smoked-peppered-beef-tenderloin.5c314418a1f75c7057eed686e2fad46f.jpg",
+            "name": "Perini Ranch Steakhouse",
+            "dsc": "Mesquite Smoked Peppered Beef Tenderloin",
+            "price": 165
+        }
+    ]
+}
